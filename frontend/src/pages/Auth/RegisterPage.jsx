3d5962f5b8d3b255061452170ea1e0d10d72ee56
Nhinguyen <238nhinguyen@gmail.com>
2025-04-15 20:57:31 +0700

    update UI auth: email,fogot,login,reg,resetpass

--- frontend/src/pages/Auth/RegisterPage.jsx
+++ frontend/src/pages/Auth/RegisterPage.jsx
@@ -91,12 +91,28 @@ const RegisterPage = () => {
           {/* Form */}
           <Form className="flex flex-col gap-4" onSubmit={handleSignUp}>
             {/* Username Field */}
-            <div>
-              <label className="block text-sm font-medium text-gray-700 mb-1">
-                USERNAME
-              </label>
+            <div className="flex flex-wrap w-full">
+              <div className="flex items-center gap-2 mb-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5  "
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                  />
+                </svg>
+                <label className="text-gray-700 font-medium">User Name</label>
+              </div>
               <Input
                 isRequired
+                // label="User Name"
+                labelPlacement="outside"
                 name="name"
                 placeholder="Enter your username"
                 type="text"
@@ -104,44 +120,75 @@ const RegisterPage = () => {
                 onChange={handleUsernameChange}
                 isInvalid={!!usernameError}
                 errorMessage={usernameError}
-                className="w-full border border-gray-300 rounded-md p-2 h-10 focus:outline-none focus:ring-2 focus:ring-black overflow-hidden text-ellipsis whitespace-nowrap"
+                className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-black"
               />
             </div>
 
             {/* Email Field */}
-            <div>
-              <label className="block text-sm font-medium text-gray-700 mb-1">
-                EMAIL
-              </label>
+            <div className="flex flex-wrap w-full">
+              <div className="flex items-center gap-2 mb-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5 "
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25"
+                  />
+                </svg>
+
+                <label className="text-gray-700 font-medium">Email</label>
+              </div>
               <Input
                 isRequired
                 errorMessage="Please enter a valid email"
+                // label="Email"
+                labelPlacement="outside"
                 name="email"
-                placeholder="example@mail.com"
+                placeholder="Enter your email"
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="w-full border border-gray-300 rounded-md p-2 h-10 focus:outline-none focus:ring-2 focus:ring-black overflow-hidden text-ellipsis whitespace-nowrap"
+                className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-black"
               />
             </div>
             {/* Password Field */}
-            <div>
-              <label className="block text-sm font-medium text-gray-700 mb-1">
-                PASSWORD
-              </label>
-              <div className="relative">
-                <CustomInputPass
-                  isRequired
-                  placeholder="••••••••"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="w-full border border-gray-300 rounded-md p-2 h-10 focus:outline-none focus:ring-2 focus:ring-black overflow-hidden text-ellipsis whitespace-nowrap"
-                />
+            <div className="flex flex-wrap w-full">
+              <div className="flex items-center gap-2 mb-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5 "
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                  />
+                </svg>
+                <label className="text-gray-700 font-medium">Password</label>
               </div>
-              {/* Password Requirements */}
-              <PasswordCriteria password={password} />
+              <CustomInputPass
+                isRequired
+                name="password"
+                placeholder="••••••••"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-black"
+                // className="w-full border border-gray-300 rounded-md p-2 h-10 focus:outline-none focus:ring-2 focus:ring-black overflow-hidden text-ellipsis whitespace-nowrap"
+              />
             </div>
 
+            {/* Password Requirements */}
+            <PasswordCriteria password={password} />
             {/* Submit Button */}
             <Button
               type="submit"
